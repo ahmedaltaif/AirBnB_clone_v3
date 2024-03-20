c1a1d711cc4d73af4f367f4f556eec032f653340
Hana00661 <eng.hana95@gmail.com>
2024-03-20 08:31:51 +0400

    more

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,7 +50,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
@@ -64,5 +64,5 @@ class FileStorage:
         """
         if obj in self.__objects.values():
             key = "{}.{}".format(type(obj).__name__, obj.id)
-            del(self.__objects[key])
+            del (self.__objects[key])
         return
